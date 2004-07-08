347be96a9f93872fdef807395314426961d0e702
tekhedd <devnull@localhost>
2004-07-08 19:37:18 +0000

    Added strong signing.

---
+++ tamjb/id3/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+// $Id$
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("byteheaven.id3")]
+[assembly: AssemblyDescription ("id3 tag management library")]
+[assembly: AssemblyCompany ("BYTE HEAVEN")]
+[assembly: AssemblyProduct("tam jukebox")]
+[assembly: AssemblyCopyright ("2004 Tom Surace")]
+[assembly: AssemblyVersion("1.0.0.*")]
+//
+// 
+//
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("../assykey.snk")]
+[assembly: AssemblyKeyName("")]
