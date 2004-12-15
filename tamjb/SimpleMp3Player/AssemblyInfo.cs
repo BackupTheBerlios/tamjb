f5042822ba1d32feff5d1474f76734adc244159a
tekhedd <devnull@localhost>
2004-12-15 10:16:32 +0000

    Everybody wants a ChangeLog.

--- tamjb/SimpleMp3Player/AssemblyInfo.cs
+++ tamjb/SimpleMp3Player/AssemblyInfo.cs
@@ -3,8 +3,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle ("byteheaven.tamjb.SimpleMp3Player")]
-[assembly: AssemblyDescription ("mp3 playback wrapper")]
+[assembly: AssemblyTitle ("byteheaven.tamjb.StreamPlayer")]
+[assembly: AssemblyDescription ("mp3/ogg playback wrapper")]
 [assembly: AssemblyCompany ("BYTE HEAVEN")]
 [assembly: AssemblyProduct("TAM Jukebox")]
 [assembly: AssemblyCopyright ("2004 Tom Surace")]
